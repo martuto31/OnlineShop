6169e1599d091111bad3d6d6837e4bf3bb874d34
Martin Petrov <martuto31@gmail.com>
2023-04-22 20:44:15 +0300

    Added ReviewEntityTypeConfiguration

---
+++ OnlineShop.DAL/Data/Configurations/ReviewEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Data.Configurations
+{
+    public class ReviewEntityTypeConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> review)
+        {
+            review.HasOne(x => x.Product)
+                .WithMany(x => x.Reviews);
+
+            review.HasOne(x => x.User)
+                .WithMany(x => x.Reviews);
+        }
+    }
+}
