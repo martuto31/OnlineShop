b6842ff4cc6819e5f9a9e71599ce60808e5afebd
Martin Petrov <martuto31@gmail.com>
2023-04-22 20:47:23 +0300

    Added RoleEntityTypeConfiguration

---
+++ OnlineShop.DAL/Data/Configurations/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Data.Configurations
+{
+    public class RoleEntityTypeConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> role)
+        {
+            role.HasMany(x => x.Users)
+                .WithOne(x => x.Role);
+        }
+    }
+}
