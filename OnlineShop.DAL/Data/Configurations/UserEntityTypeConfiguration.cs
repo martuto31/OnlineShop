cc2baf886722a60d8da66263da67a2f9fd211fa4
Martin Petrov <martuto31@gmail.com>
2023-04-22 20:46:11 +0300

    Added UserEntityTypeConfiguration

---
+++ OnlineShop.DAL/Data/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Data.Configurations
+{
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> user)
+        {
+            user.HasMany(x => x.Reviews)
+                .WithOne(x => x.User);
+
+            user.HasOne(x => x.Role)
+                .WithMany(x => x.Users);
+        }
+    }
+}
