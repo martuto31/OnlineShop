5c03f5c03e15e1320ed9962de071ceeddb1c7528
Martin Petrov <martuto31@gmail.com>
2023-04-23 15:33:41 +0300

    Added GenericRepository

---
+++ OnlineShop.DAL/Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+        where TEntity : class
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await DbSet.AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return DbSet.ToList();
+        }
+
+        public TEntity? GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        protected DbSet<TEntity> DbSet => _context.Set<TEntity>();
+    }
+}
---
+++ OnlineShop.DAL/Repository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Repository
+{
+    public interface IGenericRepository<TEntity>
+        where TEntity : class
+    {
+        TEntity? GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Add(TEntity entity);
+        Task AddAsync(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
