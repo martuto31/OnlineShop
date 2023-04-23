5ad80e0654e19c9560b163b06e0a01938a293e7c
Martin Petrov <martuto31@gmail.com>
2023-04-23 16:53:39 +0300

    Added UserRepository

---
+++ OnlineShop.DAL/Repository/User/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Repository.User
+{
+    public interface IUserRepository : IGenericRepository<Models.User>
+    {
+        Task AddUserAsync(Models.User user);
+        void DeleteUser(Models.User user);
+        void UpdateUser(Models.User user);
+        Task<Models.User?> GetUserById(int id);
+    }
+}
