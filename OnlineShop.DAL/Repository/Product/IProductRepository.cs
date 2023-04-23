3b951f93f2fe8832a6f030de5ec060f3d8b9a391
Martin Petrov <martuto31@gmail.com>
2023-04-23 15:48:40 +0300

    Added ProductRepository

---
+++ OnlineShop.DAL/Repository/Product/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.DAL.Repository.Product
+{
+    public interface IProductRepository : IGenericRepository<Models.Product>
+    {
+        Task<IEnumerable<Models.Product>> GetAllProductsAsync();
+        Task<Models.Product?> GetProductByIdAsync(int id);
+        Task AddProductAsync(Models.Product product);
+        void UpdateProduct(Models.Product product);
+        void DeleteProduct (Models.Product product);
+    }
+}
---
+++ OnlineShop.DAL/Repository/Product/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.DAL.Data;
+using OnlineShop.Models;
+
+namespace OnlineShop.DAL.Repository.Product
+{
+    public class ProductRepository : GenericRepository<Models.Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+
+        public async Task AddProductAsync(Models.Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+
+        public void DeleteProduct(Models.Product product)
+        {
+            _context.Products.Remove(product);
+        }
+
+        public async Task<IEnumerable<Models.Product>> GetAllProductsAsync()
+        {
+            return await DbSet
+                .ToListAsync();
+        }
+
+        public async Task<Models.Product?> GetProductByIdAsync(int id)
+        {
+            return await DbSet
+                .Where(x => x.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public void UpdateProduct(Models.Product product)
+        {
+            _context.Update(product);
+        }
+    }
+}
