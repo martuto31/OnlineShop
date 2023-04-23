e389ab2c8ecf7497236a9e3bb11a66000871a0fa
Martin Petrov <martuto31@gmail.com>
2023-04-23 17:06:20 +0300

    Added ProductDTO

---
+++ OnlineShop.Shared/DTO/ProductDTO/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using OnlineShop.Models;
+using OnlineShop.Models.Enums;
+
+namespace OnlineShop.Shared.DTO.ProductDTO
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public ProductTarget ProductTarget { get; set; }
+        public ProductType ProductType { get; set; }
+
+        public ICollection<Review>? Reviews { get; set; }
+    }
+}
