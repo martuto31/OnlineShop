3e3d54e96e0cc3461a97f1cba45200fe091e3f3f
Martin Petrov <martuto31@gmail.com>
2023-07-28 19:06:25 +0300

    ProductResponseDTO updated according to new Products model.

--- OnlineShop/Mapping/Models/ProductResponseDTO.cs
+++ OnlineShop/Mapping/Models/ProductResponseDTO.cs
@@ -9,12 +9,15 @@ namespace OnlineShop.Mapping.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+        public string GrowDifficulty { get; set; }
+        public string LightIntensity { get; set; }
+        public string ProductType { get; set; }
+        public bool PetCompatibility { get; set; }
+        public bool AirPurify { get; set; }
         public List<string> PicturesData { get; set; } = new List<string>();
-
-        public ProductTarget ProductTarget { get; set; }
-        public ProductType ProductType { get; set; }
 
         public List<string> ProductSizes { get; set; } = new List<string>();
         public List<string> ProductColors { get; set; } = new List<string>();
+
     }
 }
