53cff2e86a77c1e38743041147a040365423334e
Martin Petrov <martuto31@gmail.com>
2023-08-05 18:12:22 +0300

    Removed RoleId from RegisterDTO.

--- OnlineShop.Shared/DTO/UserDTO/RegisterDTO.cs
+++ OnlineShop.Shared/DTO/UserDTO/RegisterDTO.cs
@@ -11,6 +11,5 @@ namespace OnlineShop.Shared.DTO.UserDTO
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public string RoleId { get; set; }
     }
 }
