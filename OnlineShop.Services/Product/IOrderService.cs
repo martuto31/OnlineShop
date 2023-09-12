7bd98d0c674a213f3e2bec3b0e84fe6d51a665a6
Martin Petrov <martuto31@gmail.com>
2023-09-13 02:06:44 +0300

    Added all OrderService methods to IOrderService interface.

--- OnlineShop.Services/Product/IOrderService.cs
+++ OnlineShop.Services/Product/IOrderService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineShop.Models;
+using OnlineShop.Shared.DTO.OrderDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,11 @@ namespace OnlineShop.Services.Product
 {
     public interface IOrderService
     {
+        void AddOrderAsync(CreateOrderDTO orderDTO, string userId);
+        Task<Order> GetOrderByIdAsync(int id);
+        Task<IEnumerable<Order>> GetOrdersByUserIdAsync(string id);
+        Task SetDepartDate(DateTime departDate, int orderId);
+        Task SetDeliveryDate(DateTime deliveryDate, int orderId);
+        Task SetReturnDate(DateTime returnDate, int orderId);
     }
 }
