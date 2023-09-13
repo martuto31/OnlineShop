cd8584251cdef1279d241564824102697e674e23
Martin Petrov <martuto31@gmail.com>
2023-09-13 13:48:12 +0300

    Added OrderResponseDTO and OrderMapping.

---
+++ OnlineShop/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using OnlineShop.Services.Product;
+using System.Security.Claims;
+
+namespace OnlineShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly UserManager<User> _userManager;
+
+        public OrderController(IOrderService orderService, UserManager<User> userManager)
+        {
+            _orderService = orderService;
+            _userManager = userManager;
+        }
+    }
+}
---
+++ OnlineShop/Mapping/Models/OrderResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Models;
+
+namespace OnlineShop.Mapping.Models
+{
+    public class OrderResponseDTO
+    {
+        public DateTime? ShipmentDepartDate { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public bool IsShipped { get; set; }
+        public bool IsReturned { get; set; }
+        public ICollection<ProductOrder> Products { get; set; }
+    }
+}
