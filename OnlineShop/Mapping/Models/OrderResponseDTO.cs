e2f521d8fd5023e470fdbd8cfdf5e47a71377f83
Martin Petrov <martuto31@gmail.com>
2023-09-15 16:56:57 +0300

    OrderResponseDTO, CreateOrderDTO and OrderService updated.

--- OnlineShop/Mapping/Models/OrderResponseDTO.cs
+++ OnlineShop/Mapping/Models/OrderResponseDTO.cs
@@ -4,6 +4,16 @@ namespace OnlineShop.Mapping.Models
 {
     public class OrderResponseDTO
     {
+        public double OrderTotal { get; set; }
+        public double OrderWeight { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime OrderCreatedOn { get; set; }
         public DateTime? ShipmentDepartDate { get; set; }
         public DateTime? DeliveryDate { get; set; }
         public DateTime? ReturnDate { get; set; }
